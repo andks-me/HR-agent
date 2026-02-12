ee8bd400ec87c8a26ff7b543640fc040f11ef4f0
andks-me <andreeva.ks.an@gmail.com>
2026-02-12 14:18:12 +0300

    Update page_controller.ex

--- hr_brand_agent_full/lib/hr_brand_agent_web/controllers/page_controller.ex
+++ hr_brand_agent_full/lib/hr_brand_agent_web/controllers/page_controller.ex
@@ -4,7 +4,7 @@ defmodule HrBrandAgentWeb.PageController do
   def home(conn, _params) do
     # If user is logged in, redirect to dashboard
     if conn.assigns[:current_user] do
-      redirect(conn, to: ~p"/dashboard")
+      redirect(conn, to: "/dashboard")
     else
       # Show landing page
       render(conn, :home, layout: false)
